ead89cacee456c812293520f3744a617fd009a79
MarcosVargassjj <marcos.costa.vargas@escola.pr.gov.br>
2026-02-26 12:14:51 -0300

    add app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+let Aluno = "Marcos Vargas";
+let alunoidade = "16";
+let matriculado = false;
+
+if (matriculado == true) {
+    let matricula = "SIM";
+    console.log(`O aluno ${Aluno} tem ${alunoidade} anos. Status de matricula ${matricula}`);
+} else {
+    let matricula = "NÃO";
+    console.log(`O aluno ${Aluno} tem ${alunoidade} anos. Status de matricula ${matricula}`);
+}
